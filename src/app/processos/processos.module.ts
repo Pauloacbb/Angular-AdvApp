d42708e45faf961de304ca373e5613c22a3d4023
Pauloacbb <paulo.acbb@hotmail.com>
2023-05-16 15:36:56 -0300

    CSS do material table e Modulo app Material

--- src/app/processos/processos.module.ts
+++ src/app/processos/processos.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { ProcessosRoutingModule } from './processos-routing.module';
 import { ProcessosComponent } from './processos/processos.component';
 
 @NgModule({
   declarations: [ProcessosComponent],
-  imports: [CommonModule, ProcessosRoutingModule, MatTableModule],
+  imports: [CommonModule, ProcessosRoutingModule, AppMaterialModule],
 })
 export class ProcessosModule {}
